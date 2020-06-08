b8698187c41263fd4055737ba5a46116f74b8d84
ArulMozhiVaradan <jarul@gmail.com>
2020-06-08 23:15:08 +0530

    Add project files.

---
+++ LandRegistrationUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LandRegistrationUI.Startup))]
+namespace LandRegistrationUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
