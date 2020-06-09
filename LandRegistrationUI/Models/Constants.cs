0b74ac189a9cd5a74d77d836291e4198819ec84b
ArulMozhiVaradan <jarul@gmail.com>
2020-06-09 22:58:42 +0530

    Alert added and Register screen design changed

---
+++ LandRegistrationUI/Models/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LandRegistrationUI.Models
+{
+    public static class Constants
+    {
+        public static string SuccessAlert { get; set; } = "SuccessAlert";
+        public static string ErrorAlert { get; set; } = "ErrorAlert";
+        public static string WarningAlert { get; set; } = "WarningAlert";
+        public static string InfoAlert { get; set; } = "InfoAlert";
+    }
+}
