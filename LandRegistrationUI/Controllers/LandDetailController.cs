909bb05726ecda349860dae4b1e44c3b71c1be35
ArulMozhiVaradan <jarul@gmail.com>
2020-06-16 21:28:25 +0530

    Search and list view added

--- LandRegistrationUI/Controllers/LandDetailController.cs
+++ LandRegistrationUI/Controllers/LandDetailController.cs
@@ -1,6 +1,9 @@
 ﻿using LandRegistrationUI.Models;
+using LandRegistrationUI.Models.ViewModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,7 +16,102 @@ namespace LandRegistrationUI.Controllers
         // GET: LandDetail
         public ActionResult Index()
         {
-            return View();
+            LandSearchViewModel model = new LandSearchViewModel();
+            var regions = db.RegionMasters.ToList();
+            model.RegionList = regions.Select(x => new SelectListItem() { Text = x.Region, Value = x.Id.ToString() }).ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(LandSearchViewModel model)
+        {
+            model.RegionList = db.RegionMasters.Select(x => new SelectListItem() { Text = x.Region, Value = x.Id.ToString() }).ToList();
+            model.TalukList = db.TalukMasters.Where(x => x.RegionID == model.Region).Select(x => new SelectListItem() { Value = x.TalukID.ToString(), Text = x.TalukName }).ToList();
+            model.RevenueList = db.RevenueVillageMasters.Where(x => x.Id == model.Revenue).Select(x => new SelectListItem() { Value = x.Id.ToString(), Text = x.RevenueVillageName }).ToList();
+            model.WardList = db.WardMasters.Where(x => x.WardID == model.Ward).Select(x => new SelectListItem() { Value = x.WardID.ToString(), Text = x.WardName }).ToList();
+            model.BlockList = db.BlockMasters.Where(x => x.BlockID == model.Block).Select(x => new SelectListItem() { Value = x.BlockID.ToString(), Text = x.BlockName }).ToList();
+
+            model.LandDetails = db.UsersDatas
+                .Include(x => x.TalukMaster)
+                .Include(x => x.RevenueVillageMaster)
+                .Include(x => x.BlockMaster)
+                .Include(x => x.RegionMaster)
+                .Where(x =>
+                (x.RegionID == model.Region || model.Region == 0) &&
+                (x.TalukID == model.Taluk || model.Taluk == 0) &&
+                (x.RevenueVillageID == model.Revenue || model.Revenue == 0) &&
+                (x.WardID == model.Ward || model.Ward == 0) &&
+                (x.BlockID == model.Block || model.Block == 0) &&
+                (x.TSNo == model.TSNo || string.IsNullOrEmpty(model.TSNo))
+            )
+                .Select(x => new LandDetailsModel()
+                {
+                    ID = x.ID,
+                    AddressLine1 = x.AddressLine1,
+                    AddressLine2 = x.AddressLine2,
+                    AddressLine3 = x.AddressLine3,
+                    BlockID = x.BlockID,
+                    BlockName = x.BlockMaster.BlockName,
+                    OwnerName = x.OwnerName,
+                    PattaNo = x.PattaNo,
+                    PreviousOwnerAddLine1 = x.PreviousOwnerAddLine1,
+                    PreviousOwnerAddLine2 = x.PreviousOwnerAddLine2,
+                    PreviousOwnerAddLine3 = x.PreviousOwnerAddLine3,
+                    RegionID = x.RegionID,
+                    RegionName = x.RegionMaster.Region,
+                    Remarks = x.Remarks,
+                    RevenueVillageID = x.RevenueVillageID,
+                    RevenueVillageName = x.RevenueVillageMaster.RevenueVillageName,
+                    RSNo = x.RSNo,
+                    SubDivisionNo = x.SubDivisionNo,
+                    TalukID = x.TalukID,
+                    TalukName = x.TalukMaster.TalukName,
+                    TenantName = x.TenantName,
+                    TSNo = x.TSNo,
+                    WardID = x.WardID,
+                    WardName = ""
+                })
+                .ToList();
+            return View(model);
+        }
+        public JsonResult GetTaluks(int regionId)
+        {
+            var taluks = db.TalukMasters.Where(x => x.RegionID == regionId).Select(x => new { x.TalukID, x.TalukName }).ToList();
+
+            return Json(taluks, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetRevenue(int talukId)
+        {
+            var revenues = db.RevenueVillageMasters.Where(x => x.TalukID == talukId).Select(x => new { x.RevenueVillageName, x.Id }).ToList();
+
+            return Json(revenues, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetWardAndBlock(int revenueId)
+        {
+            var wards = db.WardMasters.Where(x => x.RevenueVillageID == revenueId).Select(x => new { x.WardID, x.WardName }).ToList();
+            var blocks = db.BlockMasters.Where(x => x.RevenueVillageId == revenueId).Select(x => new { x.BlockID, x.BlockName }).ToList();
+            var output = new
+            {
+                Ward = wards,
+                Block = blocks
+            };
+            return Json(output, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult GetWards(int revenueId)
+        {
+            var wards = db.WardMasters.Where(x => x.RevenueVillageID == revenueId).Select(x => new { x.WardID, x.WardName }).ToList();
+
+            return Json(wards, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetBlocks(int revenueId)
+        {
+            var blocks = db.BlockMasters.Where(x => x.RevenueVillageId == revenueId).Select(x => new { x.BlockID, x.BlockName }).ToList();
+
+            return Json(blocks, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult UserRecord()
@@ -22,10 +120,9 @@ namespace LandRegistrationUI.Controllers
             return View();
         }
 
-        public ActionResult UserDetails()
+        public ActionResult UserDetails(int id)
         {
-            var details = db.UsersDatas
-                .FirstOrDefault();
+            var details = db.UsersDatas.FirstOrDefault(x => x.ID == id);
             return View(details);
         }
     }
---
+++ LandRegistrationUI/Models/ViewModels/LandSearchViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LandRegistrationUI.Models.ViewModels
+{
+    public class LandSearchViewModel
+    {
+        public int Region { get; set; }
+        public int Taluk { get; set; }
+        public int Revenue { get; set; }
+        public string LGR { get; set; }
+        public int Ward { get; set; }
+        public int Block { get; set; }
+        public string TSNo { get; set; }
+        public string RSNo { get; set; }
+        public string SubDivision { get; set; }
+        public List<LandDetailsModel> LandDetails { get; set; }
+        public IEnumerable<SelectListItem> RegionList { get; set; } = new List<SelectListItem>();
+        public IEnumerable<SelectListItem> TalukList { get; set; } = new List<SelectListItem>();
+        public IEnumerable<SelectListItem> RevenueList { get; set; } = new List<SelectListItem>();
+        public IEnumerable<SelectListItem> WardList { get; set; } = new List<SelectListItem>();
+        public IEnumerable<SelectListItem> BlockList { get; set; } = new List<SelectListItem>();
+
+    }
+
+    public class LandDetailsModel
+    {
+        public int ID { get; set; }
+        public byte RegionID { get; set; }
+        public string RegionName { get; set; }
+        public int TalukID { get; set; }
+        public string TalukName { get; set; }
+        public int? WardID { get; set; }
+        public string WardName { get; set; }
+        public int? BlockID { get; set; }
+        public string BlockName { get; set; }
+        public int RevenueVillageID { get; set; }
+        public string RevenueVillageName { get; set; }
+        public string TSNo { get; set; }
+        public string SubDivisionNo { get; set; }
+        public string RSNo { get; set; }
+        public string PattaNo { get; set; }
+        public string OwnerName { get; set; }
+        public string TenantName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string PreviousOwnerAddLine1 { get; set; }
+        public string PreviousOwnerAddLine2 { get; set; }
+        public string PreviousOwnerAddLine3 { get; set; }
+        public string Remarks { get; set; }
+    }
+}
